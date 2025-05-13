c01038f71af401c6ea29e518553e965e17c3f236
Vladyslav Li <vladyslav_li@jabil.com>
2025-05-13 23:07:57 +0200

    add products

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -3,18 +3,26 @@ import { inject, Injectable } from "@angular/core";
 import { map, Observable } from "rxjs";
 
 export interface Product {
-  title: string;
+  brand: string;
+  name: string;
   description: string;
+  images: string[],
+  prices: {
+    price: string;
+    available: number;
+    size?: string;
+  }[],
+  note?: string;
 }
 
-export type ProductsResponse = { products: Product[] };
+type ProductsResponse = { products: Product[] };
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   private readonly http = inject(HttpClient);
 
   public getProducts(): Observable<Product[]> {
-    return this.http.get<ProductsResponse>('/assets/products.json')
+    return this.http.get<ProductsResponse>('/assets/products/products.json')
       .pipe(map(({ products }) => products ));
   }
 }
